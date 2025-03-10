4cd8412e959731317fce522b3da32623749414b9
Fawwaz <fawwaz-1427@hotmail.com>
2025-03-10 16:01:07 +0800

    feat(web,server): add CORS and client-side data fetching demo

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,17 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
+
+app.use('*', cors({
+  origin: ['http://localhost:3000', "https://cloudflare-vercel-mix-web.vercel.app"],
+  allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+  allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
+  exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+  maxAge: 600,
+  credentials: true,
+}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
